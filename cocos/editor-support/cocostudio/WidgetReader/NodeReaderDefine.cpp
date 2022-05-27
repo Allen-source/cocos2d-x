a65d3c442df136c242d02c21ee9c2e37f4796c46
王若冲 <ruochong.wang@me2zen.com>
2022-05-27 15:32:26 +0800

    create repo

---
+++ cocos/editor-support/cocostudio/WidgetReader/NodeReaderDefine.cpp
@@ -0,0 +1,5 @@
+
+
+#include "editor-support/cocostudio/WidgetReader/NodeReaderDefine.h"
+#if CC_USE_COCOSTUDIO
+#endif
---
+++ cocos/editor-support/cocostudio/WidgetReader/NodeReaderProtocol.cpp
@@ -0,0 +1,11 @@
+//
+//  NodeReaderProtocol.cpp
+//  cocos2d_libs
+//
+//  Created by pipu on 14/11/18.
+//
+//
+
+#include "editor-support/cocostudio/WidgetReader/NodeReaderProtocol.h"
+#if CC_USE_COCOSTUDIO
+#endif
